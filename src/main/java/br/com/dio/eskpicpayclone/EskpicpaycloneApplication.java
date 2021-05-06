32cd71d071e20817bd38833f4c98d25d08620dc8
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-06 07:31:32 -0300

    Inicial commit project PicPay Clone

---
+++ src/main/java/br/com/dio/eskpicpayclone/EskpicpaycloneApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.eskpicpayclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskpicpaycloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskpicpaycloneApplication.class, args);
+	}
+
+}
