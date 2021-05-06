0648b5b74992700e31b9777135428f06fe545152
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-06 09:44:45 -0300

    Create Advice Exception

---
+++ src/main/java/br/com/dio/eskpicpayclone/exceptions/RestControllerAdviceException.java
@@ -0,0 +1,37 @@
+package br.com.dio.eskpicpayclone.exceptions;
+
+import br.com.dio.eskpicpayclone.dto.ErrorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class RestControllerAdviceException {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErrorDTO> handle(MethodArgumentNotValidException exception){
+        List<ErrorDTO> errorDTOS = new ArrayList<>();
+
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(e -> {
+            String message = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+            ErrorDTO errorDTO = new ErrorDTO(e.getField(), message);
+            errorDTOS.add(errorDTO);
+        });
+
+        return errorDTOS;
+    }
+}
